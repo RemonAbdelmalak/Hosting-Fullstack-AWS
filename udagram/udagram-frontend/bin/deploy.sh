edee79e9bbdae78d1db66c11946c1274ec7d4a00
Remon Abdelmalak <remonwagdy1999@gmail.com>
2023-03-06 18:33:08 +0200

    Deploy Fix

--- udagram/udagram-frontend/bin/deploy.sh
+++ udagram/udagram-frontend/bin/deploy.sh
@@ -1,1 +1,2 @@
 aws s3 cp --recursive --acl public-read ./www s3://projectbucket24176939246/
+aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./www/index.html s3://projectbucket241769392460/
